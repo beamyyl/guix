197aa07304e580e34a0e3cb7d4e99fff1b993280
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-06-09 22:13:09 +0300

    Update setupguix.sh

--- setupguix.sh
+++ setupguix.sh
@@ -4,4 +4,4 @@ echo 'alias gpull="guix shell git -- git clone --depth=1 https://codeberg.org/gu
 echo 'export PATH="$HOME/.config/guix/current/bin:$PATH"' | sudo tee -a /root/.bash_profile
 echo 'alias gpull="guix shell git -- git clone --depth=1 https://codeberg.org/guix/guix.git /tmp/guix-fast && guix pull --allow-downgrades --disable-authentication"' | sudo tee -a /root/.bash_profile
 sudo cp channels.scm /etc/guix/channels.scm
-echo "Pull by runnning 'gpull' and then rebuild by running the 'firstreconfigure.sh' script"!
+echo "Pull by runnning 'gpull', 'hash guix' and then rebuild by running the 'firstreconfigure.sh' script"!
