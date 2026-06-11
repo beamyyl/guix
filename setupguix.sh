d6815d7ec4a660bac0be100c5491e5f61d27bc84
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-06-11 15:32:30 +0300

    Update setupguix.sh

--- setupguix.sh
+++ setupguix.sh
@@ -1,7 +1,25 @@
-sudo wget https://substitutes.nonguix.org/signing-key.pub -O /etc/nonguix-signing-key.pub
+sudo wget https://nonguix.org -O /etc/nonguix-signing-key.pub
+
 echo 'export PATH="$HOME/.config/guix/current/bin:$PATH"' | sudo tee -a /root/.bashrc
-echo 'alias gpull="rm -rf /tmp/guix-fast /root/.cache/guix && git clone --depth=1 https://codeberg.org/guix/guix.git /tmp/guix-fast && guix pull --allow-downgrades --disable-authentication"' | sudo tee -a /root/.bashrc
+echo 'alias gpull="rm -rf /tmp/guix-fast /root/.cache/guix && git clone --depth=1 https://codeberg.org /tmp/guix-fast && guix pull --allow-downgrades --disable-authentication"' | sudo tee -a /root/.bashrc
 echo 'export PATH="$HOME/.config/guix/current/bin:$PATH"' | sudo tee -a /root/.bash_profile
-echo 'alias gpull="rm -rf /tmp/guix-fast /root/.cache/guix && git clone --depth=1 https://codeberg.org/guix/guix.git /tmp/guix-fast && guix pull --allow-downgrades --disable-authentication"' | sudo tee -a /root/.bash_profile
+echo 'alias gpull="rm -rf /tmp/guix-fast /root/.cache/guix && git clone --depth=1 https://codeberg.org /tmp/guix-fast && guix pull --allow-downgrades --disable-authentication"' | sudo tee -a /root/.bash_profile
 sudo cp channels.scm /etc/guix/channels.scm
-echo "Pull by runnning 'gpull', 'hash guix' and then rebuild by running the 'firstreconfigure.sh' script"!
+
+sudo tee -a /etc/config.scm << 'EOF'
+(set! %operating-system-services
+      (let ((old-services %operating-system-services))
+        (lambda (os)
+          (modify-services (old-services os)
+            (guix-service-type config =>
+                               (guix-configuration
+                                (inherit config)
+                                (substitute-urls
+                                 (append (list "https://nonguix.org")
+                                         %default-substitute-urls))
+                                (authorized-keys
+                                 (append (list (local-file "/etc/nonguix-signing-key.pub"))
+                                         %default-authorized-guix-keys))))))))
+EOF
+
+echo "Pull by running 'gpull', 'hash guix' and then rebuild by running the 'firstreconfigure.sh' script!"
