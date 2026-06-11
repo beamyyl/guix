9121dfb17a3baefa7e7d99b5d6a0fdb2c056270e
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-06-11 15:36:57 +0300

    Update setupguix.sh

--- setupguix.sh
+++ setupguix.sh
@@ -1,25 +1,32 @@
 sudo wget https://nonguix.org -O /etc/nonguix-signing-key.pub
-
 echo 'export PATH="$HOME/.config/guix/current/bin:$PATH"' | sudo tee -a /root/.bashrc
 echo 'alias gpull="rm -rf /tmp/guix-fast /root/.cache/guix && git clone --depth=1 https://codeberg.org /tmp/guix-fast && guix pull --allow-downgrades --disable-authentication"' | sudo tee -a /root/.bashrc
 echo 'export PATH="$HOME/.config/guix/current/bin:$PATH"' | sudo tee -a /root/.bash_profile
 echo 'alias gpull="rm -rf /tmp/guix-fast /root/.cache/guix && git clone --depth=1 https://codeberg.org /tmp/guix-fast && guix pull --allow-downgrades --disable-authentication"' | sudo tee -a /root/.bash_profile
 sudo cp channels.scm /etc/guix/channels.scm
 
-sudo tee -a /etc/config.scm << 'EOF'
-(set! %operating-system-services
-      (let ((old-services %operating-system-services))
-        (lambda (os)
-          (modify-services (old-services os)
-            (guix-service-type config =>
-                               (guix-configuration
-                                (inherit config)
-                                (substitute-urls
-                                 (append (list "https://nonguix.org")
-                                         %default-substitute-urls))
-                                (authorized-keys
-                                 (append (list (local-file "/etc/nonguix-signing-key.pub"))
-                                         %default-authorized-guix-keys))))))))
-EOF
+if grep -q "%desktop-services" /etc/config.scm; then
+    guix shell sd -- sd '(%desktop-services))' '(%desktop-services)
+     (guix-service-type config =>
+                        (guix-configuration
+                         (inherit config)
+                         (substitute-urls
+                          (append (list "https://nonguix.org")
+                                  %default-substitute-urls))
+                         (authorized-keys
+                          (append (list (local-file "/etc/nonguix-signing-key.pub"))
+                                  %default-authorized-guix-keys))))' /etc/config.scm -i
+else
+    guix shell sd -- sd '(%base-services))' '(%base-services)
+     (guix-service-type config =>
+                        (guix-configuration
+                         (inherit config)
+                         (substitute-urls
+                          (append (list "https://nonguix.org")
+                                  %default-substitute-urls))
+                         (authorized-keys
+                          (append (list (local-file "/etc/nonguix-signing-key.pub"))
+                                  %default-authorized-guix-keys))))' /etc/config.scm -i
+fi
 
-echo "Pull by running 'gpull', 'hash guix' and then rebuild by running the 'firstreconfigure.sh' script!"
+echo "Pull by runnning 'gpull', 'hash guix' and then rebuild by running the 'firstreconfigure.sh' script"!
