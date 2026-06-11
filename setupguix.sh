34806c2cd3d1af5b31371a23a25b8d6e999b4b5f
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-06-11 16:07:14 +0300

    Update setupguix.sh

--- setupguix.sh
+++ setupguix.sh
@@ -1,4 +1,4 @@
-sudo wget https://nonguix.org -O /etc/nonguix-signing-key.pub
+sudo wget https://substitutes.nonguix.org/signing-key.pub -O /etc/nonguix-signing-key.pub
 echo 'export PATH="$HOME/.config/guix/current/bin:$PATH"' | sudo tee -a /root/.bashrc
 echo 'alias gpull="rm -rf /tmp/guix-fast /root/.cache/guix && git clone --depth=1 https://codeberg.org /tmp/guix-fast && guix pull --allow-downgrades --disable-authentication"' | sudo tee -a /root/.bashrc
 echo 'export PATH="$HOME/.config/guix/current/bin:$PATH"' | sudo tee -a /root/.bash_profile
@@ -11,7 +11,7 @@ sudo awk '
         match($0, /%[a-z\-]+services/)
         service_name = substr($0, RSTART, RLENGTH)
 
-        gsub(/%[a-z\-]+services/, "(modify-services " service_name "\n     (guix-service-type config =>\n                        (guix-configuration\n                         (inherit config)\n                         (substitute-urls\n                          (append (list \"https://nonguix.org\")\n                                  %default-substitute-urls))\n                         (authorized-keys\n                          (append (list (local-file \"/etc/nonguix-signing-key.pub\"))\n                                  %default-authorized-guix-keys)))))", $0)
+        gsub(/%[a-z\-]+services/, "(modify-services " service_name "\n     (guix-service-type config =>\n                        (guix-configuration\n                         (inherit config)\n                         (substitute-urls\n                          (append (list \"https://substitutes.nonguix.org\")\n                                  %default-substitute-urls))\n                         (authorized-keys\n                          (append (list (local-file \"/etc/nonguix-signing-key.pub\"))\n                                  %default-authorized-guix-keys)))))", $0)
     }
     print $0
 }' /etc/config.scm > /tmp/config.tmp && sudo mv /tmp/config.tmp /etc/config.scm
